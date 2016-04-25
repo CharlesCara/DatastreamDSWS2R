58346dc04a3573851751e2c415f19c992854d4d4
Charles Cara <charles.cara@pobox.com>
2016-04-25 13:27:30 +0100

    Changed UCTS server to product.datastream.com

--- R/UCTSUpload.R
+++ R/UCTSUpload.R
@@ -139,7 +139,7 @@ UCTSUpload <- function(tsData,
                        PrimeCurr="",
                        strUsername=options()$Datastream.Username,
                        strPassword=options()$Datastream.Password,
-                       strServerName="http://datastream.thomsonreuters.com",
+                       strServerName="http://product.datastream.com",
                        strServerPage="/UCTS/UCTSMaint.asp"){
 
   #Check inputs are valid
